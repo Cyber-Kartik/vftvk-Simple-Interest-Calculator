16442e1c841f2b7e319ec236e2c76a2b15d84acc
Kartik <64240094+Cyber-Kartik@users.noreply.github.com>
2021-07-19 22:22:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 function calculate()
 {
+  var p = document.getElementById ("principal").value;
+  if(p==""){
+  
+    alert("Please enter a positive number");
+   
+    document.getElementById("principal").focus();
+  }else{
 
-var p = document.getElementById ("principal").value;
+
+var dep = document.getElementById("dep");
+
+dep.innerHTML ="If you deposit " + "<span class='highlight'>" + p + "</span>" +",";
+
 var t = document.getElementById ("myRange").value;
-var r = document.getElementById ("txtprice").value;
+var rt = document.getElementById("rt");
+rt.innerHTML ="at an interest rate of " + "<span class='highlight'>" + t + "</span>" +"%.";
+
+
+var r = document.getElementById ("cmbitems").value;
+var cur_yr = new Date().getFullYear();
+var year = parseInt(cur_yr)+ parseInt(r);
+var yr = document.getElementById("yr");
+yr.innerHTML ="in the year <span class='highlight'>" + year + "</span>.";
+
 var result = document.getElementById("result");
-result.innerHTML = "The interest is " + ((p*t*r)/100 );
+result.innerHTML = "You will receive an amount of  " +  "<span class='highlight'>" +  ((p*t*r)/100 ) + "</span>";
+}
+}
+function alrt()
+{
+  var p = document.getElementById ("principal").value;
+  
+
+
+if(parseInt(p)==0 || parseInt(p)<0){
+  
+  alert("Please enter a positive number");
+  document.getElementById("principal").value='';
+  document.getElementById("principal").focus();
+}
+
 
 }
 
 
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var output = document.getElementById("demo") ;
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
-}
-var select = document.getElementById('cmbitems');
-var input = document.getElementById('txtprice');
-select.onchange = function() {
-    input.value = select.value;
 }
